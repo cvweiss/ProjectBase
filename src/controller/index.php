6d3aca070cfdb0c8257109ae7dd68ddc85e32eb6
Clinton Weiss <cvweiss@gmail.com>
2016-05-17 15:49:57 +0000

    scrutinizer improvements

--- src/controller/index.php
+++ src/controller/index.php
@@ -6,7 +6,7 @@ class index
 {
     public function doGet($render, $params)
     {
-        $params['content'] => 'Hello World';
+        $params['content'] = 'Hello World';
         $params['title'] = 'Home Page';
 
         $render->render("index", $params);
