26eeb86bfb95f1dfbda1ddb2721b4a8ee6a6d4a5
Clinton Weiss <cvweiss@gmail.com>
2016-05-18 01:11:47 +0000

    detects directory of project

--- src/config.php
+++ src/config.php
@@ -2,17 +2,21 @@
 
 namespace Project\Base;
 
+$vendorDir = dirname(dirname(dirname(__DIR__)));
+// Composer insists that libs are kept in vendor, so we'll make that assumption
+$projectDir = basename($vendorDir) == 'vendor' ? dirname($vendorDir) : dirname(__DIR__);
+
 Config::setAll([
 	'debug' => true,
 	'siteName' => 'Project Base',
 
 	// Project settings
-	'projectDir' => realpath(__DIR__ . '/../'),
+	'projectDir' => $projectDir
 ]);
 
-if (file_exists(__DIR__ . '/../config.json'))
+if (file_exists($projecDir . '/config.json'))
 {
-    $raw = file_get_contents(__DIR__ . '/../config.json');
+    $raw = file_get_contents($projectDir . '/config.json');
     $config = json_decode($raw, true);
     foreach ($config as $key=>$value) Config::set($key, $value, true);
 }
