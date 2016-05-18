2855410232bcb31635e5c101e757f7e354c5f88c
Clinton Weiss <cvweiss@gmail.com>
2016-05-18 02:26:22 +0000

    looks for template in view of projectDir or vendor code dir

--- src/jade.php
+++ src/jade.php
@@ -2,10 +2,12 @@
 
 namespace Project\Base;
 
+$paths = [Config::get('projectDir') . '/view/', Config::get('projectDir') . '/vendor/cvweiss/project.base/view/'];
+
 $jade = new \Tale\Jade\Renderer([
-	'paths' => [Config::get('projectDir') . '/view/'],
+	'paths' => $paths,
 	'pretty' => Config::get('debug', false),
-        'cachePath' => Config::get('projectDir') . '/cache/jade/',
+        'cachePath' => Config::get('cachePath', Config::get('projectDir') . '/cache/jade/'),
 ]);
 
 $view = new Render($jade);
