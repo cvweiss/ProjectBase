10754d41796c86d4a27892f7228bb5418672cc03
Clinton Weiss <cvweiss@gmail.com>
2016-05-19 18:47:29 +0000

    some cleanup

--- src/controller/auth/google/callback.php
+++ src/controller/auth/google/callback.php
@@ -4,15 +4,15 @@ namespace Project\Base\Controller\auth\google;
 
 use Project\Base\Session;
 use Project\Base\Db;
+use Project\Base\Config;
+use Project\Base\Logger;
 
 class callback
 {
     public function doGet($view, $params)
     { 
-        $auth = \Project\Base\Config::get("oauth2");
-
+        $auth = Config::get("oauth2");
         $google = $auth['google'];
-
         $provider = new \League\OAuth2\Client\Provider\Google([
                 'clientId'     => $google['client_id'],
                 'clientSecret' => $google['client_secret'],
@@ -20,11 +20,7 @@ class callback
         ]);
 
         $session = Session::getSession();
-        if (!empty($_GET['error'])) {
-            // Got an error, probably user denied access
-            $view->redirect('/logout/');
-
-        } elseif (empty($_GET['state']) || ($_GET['state'] !== $session->get('oauth2state'))) {
+        if (!empty($_GET['error']) || (empty($_GET['state']) || ($_GET['state'] !== $session->get('oauth2state')))) {
             $view->redirect('/logout/');
         } else {
             // Try to get an access token (using the authorization code grant)
@@ -34,26 +30,23 @@ class callback
                 $view->redirect('/auth/google/login', 302);
             }
 
-
             // We got an access token, let's now get the owner details
             $ownerDetails = $provider->getResourceOwner($token);
 
-            // Details
-            $id = $ownerDetails->getID();
-            $email = $ownerDetails->getEmail();
-            $name = $ownerDetails->getName();
-            $image = $ownerDetails->getAvatar();
-
-            $user = Db::get()->users->findOne(['id' => $id]);
+            // Create (if necessary) and store the user
+            $user = Db::get()->users->findOne(['id' => $ownerDetails->getID()]);
             if ($user == null) $user = new \MongoDB\Model\BSONDocument;
-            $user->id = $id;
-            $user->name = $name;
-            $user->email = $email;
-            $user->image = $image;
+
+            $user->id = $ownerDetails->getID();
+            $user->name = $ownerDetails->getName();
+            $user->email = $ownerDetails->getEmail();
+            $user->image = $ownerDetails->getAvatar();
             $user->oauth2 = "google";
             $options = isset($user->_id) ? [] : ['upsert' => true];
             Db::get()->users->replaceOne($user, $user, $options);
 
+            // User is logged in
+            Logger::info($user->name . " (" . $user->email . ") has logged in.");
             $session->set("userID", $user->id);
             $view->redirect('/', 302);
         }
