3791bbbd123b72a164bf01c281af6c1568d150e4
Clinton Weiss <cvweiss@gmail.com>
2016-05-19 19:37:19 +0000

    wrong directory...

--- src/classes/Setup.php
+++ src/classes/Setup.php
@@ -8,7 +8,7 @@ class Setup
     {
         $vendorDir = dirname(dirname(dirname(__DIR__)));
         // Composer insists that libs are kept in vendor, so we'll make that assumption
-        $projectDir = basename($vendorDir) == 'vendor' ? dirname($vendorDir) : dirname(__DIR__);
+        $projectDir = basename($vendorDir) == 'vendor' ? dirname($vendorDir) : dirname(dirname(__DIR__));
 
         $dirs = ['/view/', '/cache/', '/cache/jade'];
         foreach ($dirs as $dir) self::makeDir($projectDir . $dir);
