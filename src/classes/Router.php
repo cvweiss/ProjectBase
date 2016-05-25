3a3efa3da2260e8c9e7b66871f96136c1852220b
Clinton Weiss <cvweiss@gmail.com>
2016-05-25 15:12:57 +0000

    renamed args to params

--- src/classes/Router.php
+++ src/classes/Router.php
@@ -9,32 +9,32 @@ class Router
         $uri = filter_input(INPUT_SERVER, 'REQUEST_URI');
         $method = ucfirst(strtolower(filter_input(INPUT_SERVER, 'REQUEST_METHOD')));
         $call = "do$method";
-        $args = Config::getAll();
+        $params = Config::getAll();
 
         $ex = explode('?', $uri);
         $uri = $ex[0] == '/' ? 'index' : $ex[0];
-        $args['title'] = $uri;
+        $params['title'] = $uri;
 
         $ex = explode('/', $uri);
         $ex = array_diff($ex, ['']);
         while (sizeof($ex) > 0)
         {
             $className = '\\Project\\Base\\Controller\\' . implode('\\', $ex);
-            $this->routeCall($className, $call, $view, $args);
-            array_unshift($args, array_pop($ex));
+            $this->routeCall($className, $call, $view, $params);
+            array_unshift($params, array_pop($ex));
         }
 
         Logger::debug("404 $uri");
-        $view->error(404, "$uri could not be found", $args);
+        $view->error(404, "$uri could not be found", $params);
     }
 
-    protected function routeCall($className, $call, $view, $args)
+    protected function routeCall($className, $call, $view, $params)
     {
         if (class_exists($className))
         {   
             Logger::debug("200 $className $call");
             $class = new $className();
-            $class->$call($view, $args);
+            $class->$call($view, $params);
             throw new \Exception("Called $className::$call but code did not terminate as expected.");
         }
     }
