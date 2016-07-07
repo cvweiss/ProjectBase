d805a9f8e2804bd023cbd8f4f7bbed5ac5689353
Clinton Weiss <cvweiss@gmail.com>
2016-07-07 20:35:04 +0000

    setup checks

--- src/classes/Setup.php
+++ src/classes/Setup.php
@@ -6,6 +6,7 @@ class Setup
 {
     public static function prepareProject()
     {
+        echo "Preparing project\n";
         $vendorDir = dirname(dirname(dirname(__DIR__)));
         // Composer insists that libs are kept in vendor, so we'll make that assumption
         $projectDir = basename($vendorDir) == 'vendor' ? dirname($vendorDir) : dirname(dirname(__DIR__));
@@ -16,6 +17,7 @@ class Setup
 
     private static function makeDir($dir)
     {
+        echo "Checking for $dir\n";
         if (is_dir($dir)) return;
         echo "Creating directory: $dir\n";
         mkdir($dir);
